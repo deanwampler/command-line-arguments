6575d1e90a17bb6ec688df219698b3e23995c7e4
Dean Wampler <dean@concurrentthought.com>
2015-01-02 09:17:57 -0600

    Adding sonatype release support to the build.

---
+++ build.sbt
@@ -0,0 +1,35 @@
+import SonatypeKeys._
+
+initialCommands += "import com.concurrentthought.cla._"
+
+// See https://github.com/xerial/sbt-sonatype
+sonatypeSettings
+
+organization := "com.concurrentthought.cla"
+
+profileName := "com.concurrentthought"
+
+version := "0.1.0"
+
+pomExtra := {
+  <url>https://github.com/deanwampler/command-line-arguments</url>
+  <licenses>
+    <license>
+      <name>Apache 2</name>
+      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>github.com/deanwampler/command-line-arguments</url>
+    <connection>scm:git:github.com/deanwampler/command-line-arguments</connection>
+    <developerConnection>scm:git:git@github.com:deanwampler/command-line-arguments</developerConnection>
+  </scm>
+  <developers>
+    <developer>
+      <id>deanwampler</id>
+      <name>Dean Wampler</name>
+      <url>http://concurrentthought.com</url>
+    </developer>
+  </developers>
+}
--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,42 @@
 import sbt._
 import sbt.Keys._
 
+import com.typesafe.sbt.SbtGit._
+import GitKeys._
+import sbtbuildinfo.Plugin._
+
+import sbtrelease._
+import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
+import sbtrelease.Utilities._
+
 object BuildSettings {
 
   val Name = "command-line-arguments"
   val Version = "0.1.0"
-  val ScalaVersion = "2.11.4"
+  val ScalaVersion  = "2.11.4"
 
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq (
-    name          := Name,
-    version       := Version,
-    scalaVersion  := ScalaVersion,
-    organization  := "com.concurrentthought",
-    description   := "A library for handling command-line arguments.",
-    scalacOptions := Seq("-deprecation", "-unchecked", "-encoding", "utf8", "-Xlint")
-  )
+  lazy val buildSettings =
+    Defaults.coreDefaultSettings ++ buildInfoSettings ++ releaseSettings ++ Seq (
+      name          := Name,
+      version       := Version,
+      scalaVersion  := ScalaVersion,
+      description   := "A library for handling command-line arguments.",
+      scalacOptions := Seq("-deprecation", "-unchecked", "-encoding", "utf8",
+        "-Xlint", "-feature", "-Ywarn-infer-any"), //, "-Ywarn-value-discard"),
+
+      buildInfoPackage := Name,
+      buildInfoKeys := Seq[BuildInfoKey](version, scalaVersion),
+      buildInfoKeys ++= Seq[BuildInfoKey](
+        version,
+        scalaVersion,
+        gitHeadCommit,
+        BuildInfoKey.action("buildTime") {
+          System.currentTimeMillis
+        }
+      )
+    )
 }
 
 object Resolvers {
@@ -27,7 +49,7 @@ object Resolvers {
 
 object Dependencies {
 
-  val scalaTest      = "org.scalatest"  %% "scalatest"    % "2.2.1"  % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
 
   val dependencies = Seq(scalaTest)
 }
@@ -37,7 +59,7 @@ object CLABuild extends Build {
   import Dependencies._
   import BuildSettings._
 
-  lazy val activatorspark = Project(
+  lazy val claProject = Project(
     id = BuildSettings.Name,
     base = file("."),
     settings = buildSettings ++ Seq(
